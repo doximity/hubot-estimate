acd43735b906259fb06b24b30a81553e7c20d18a
James Klein <kleinjm007@gmail.com>
2018-01-26 18:20:53 -0500

    remove modules and bump version

--- node_modules/hubot-test-helper/test/scripts/enter-leave.js
+++
@@ -1,7 +0,0 @@
-// Description:
-//   Test script
-module.exports = function(robot) {
-  robot.enter(res => res.send(`Hi ${res.message.user.name}!`));
-
-  return robot.leave(res => res.send(`Bye ${res.message.user.name}!`));
-};
--- node_modules/hubot-test-helper/test/scripts/hello-world-listener.js
+++
@@ -1,4 +0,0 @@
-// Description:
-//   Test script
-module.exports = robot =>
-  robot.listen(message => message.user.name === 'bob', response => response.reply('hi'))
